f578727007adbc2980c721b22bbb3dfd292ec0b2
Vinicius D. Cerutti <51954708+viniciusdc@users.noreply.github.com>
2021-05-21 15:31:28 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from distutils.errors import CompileError, LinkError
 from numpy.distutils.command.build_ext import build_ext
 
 
-DISTNAME = 'scikit-image-testviniver'
+DISTNAME = 'scikit-image-testvinivermacos'
 DESCRIPTION = 'Just a test'
 MAINTAINER = 'Stefan van der Walt'
 MAINTAINER_EMAIL = 'stefan@sun.ac.za'
