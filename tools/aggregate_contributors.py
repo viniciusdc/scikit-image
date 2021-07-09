af4ab28c71a9d62c9476293b07b5c670825291bc
viniciusdc <vinivdc2009@hotmail.com>
2021-07-09 11:34:41 -0300

    Add Towncrier files

---
+++ tools/aggregate_contributors.py
@@ -0,0 +1,156 @@
+import os
+import argparse
+from datetime import datetime
+from collections import OrderedDict
+import string
+from warnings import warn
+
+from github import Github
+from github import GithubException
+try:
+    from tqdm import tqdm
+except ImportError:
+    from warnings import warn
+    warn('tqdm not installed. This script takes approximately 5 minutes '
+         'to run. To view live progressbars, please install tqdm. '
+         'Otherwise, be patient.')
+    def tqdm(i, **kwargs):
+        return i
+
+parser = argparse.ArgumentParser(usage=__doc__)
+parser.add_argument('path', help='Curent directory.')
+args = parser.parse_args()
+
+def get_commits(repo, users, reviewers, dir):
+    upcomming_changes_path = os.path.join(dir, 'doc','source', 'upcoming_changes')
+    prs_list = []
+    for file in os.listdir(upcomming_changes_path):
+        pr_number = file.split('.')[0]
+        if os.path.isfile(os.path.join(upcomming_changes_path, file)) and pr_number.isnumeric():
+            prs_list.append(pr_number)
+    
+    if 'towncrier_template' in prs_list:
+        prs_list.remove('towncrier_template')
+
+    if not prs_list:
+        # this list is empty, no significant changes to adress.
+        print('No significant changes.')
+        return
+    else:
+        print('Getting all commits from upcomming changes...')
+    
+        all_commits = list()
+        for pr_number in prs_list:
+            try:
+                pr = repo.get_pull(int(pr_number))
+            except GithubException as e:
+                print(str(e))
+                continue
+            all_commits += [item for item in pr.get_commits()]
+            for review in pr.get_reviews():
+                if review.user.login not in users:
+                    users[review.user.login] = review.user.name
+                reviewers.add(users[review.user.login])
+        return all_commits
+        
+def find_author_info(commit):
+    """Return committer and author of a commit.
+
+    Parameters
+    ----------
+    commit : Github commit
+        The commit to query.
+
+    Returns
+    -------
+    committer : str or None
+        The git committer.
+    author : str
+        The git author.
+    """
+    committer = None
+    if commit.committer is not None:
+        committer = commit.committer.name or commit.committer.login
+    git_author = commit.raw_data['commit']['author']['name']
+    if commit.author is not None:
+        author = commit.author.name or commit.author.login + f' ({git_author})'
+    else:
+        # Users that deleted their accounts will appear as None
+        author = git_author
+    return committer, author
+
+
+def add_to_users(users, new_user):
+    if new_user.name is None:
+        users[new_user.login] = new_user.login
+    else:
+        users[new_user.login] = new_user.name
+
+
+def main(args):
+    dir = args.path
+    GH_USER = 'scikit-image'
+    GH_REPO = 'scikit-image'
+    GH_TOKEN = os.environ.get('GH_TOKEN')
+    if GH_TOKEN is None:
+        raise RuntimeError(
+            "It is necessary that the environment variable `GH_TOKEN` "
+            "be set to avoid running into problems with rate limiting. "
+            "One can be acquired at https://github.com/settings/tokens.\n\n"
+            "You do not need to select any permission boxes while generating "
+            "the token.")
+
+    g = Github(GH_TOKEN)
+    repository = g.get_repo(f'{GH_USER}/{GH_REPO}')
+    
+    authors = set()
+    reviewers = set()
+    committers = set()
+    users = dict()  # keep track of known usernames
+
+    all_commits = get_commits(repository, users, reviewers, dir)
+
+    # find_author_info(dir, repository)
+    for commit in tqdm(all_commits, desc='Getting commiters and authors'):
+        committer, author = find_author_info(commit)
+        if committer is not None:
+            committers.add(committer)
+            # users maps github ids to a unique name.
+            add_to_users(users, commit.committer)
+            committers.add(users[commit.committer.login])
+
+        if commit.author is not None:
+            add_to_users(users, commit.author)
+        authors.add(author)
+
+    # this gets found as a commiter
+    committers.discard('GitHub Web Flow')
+    authors.discard('Azure Pipelines Bot')
+
+
+    contributors = OrderedDict()
+
+    contributors['authors'] = authors
+    # contributors['committers'] = committers
+    contributors['reviewers'] = reviewers
+
+    with open('reviewers_and_authors.txt', 'w+') as file:
+        for section_name, contributor_set in contributors.items():
+            file.write('\n')
+            committer_str = (f'{len(contributor_set)} {section_name} added to this '
+                     'release [alphabetical by first name or login]\n')
+            file.write(committer_str)
+            file.write('-' * len(committer_str))
+            file.write('\n')
+
+            # Remove None from contributor set if it's in there.
+            if None in contributor_set:
+                contributor_set.remove(None)
+
+            for c in sorted(contributor_set, key=str.lower):
+                file.write(f'- {c} \n')
+            file.write('\n')
+
+if __name__ == '__main__':
+    main(args)
+    
