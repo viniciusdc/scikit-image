6c7e528c420ac69b94042889c862c46ea08c20e9
leej3 <johnleenimh@gmail.com>
2021-07-12 10:41:32 +0100

    tidy with black

--- tools/aggregate_contributors.py
+++ tools/aggregate_contributors.py
@@ -13,35 +13,42 @@ try:
     from tqdm import tqdm
 except ImportError:
     from warnings import warn
-    warn('tqdm not installed. This script takes approximately 5 minutes '
-         'to run. To view live progressbars, please install tqdm. '
-         'Otherwise, be patient.')
+
+    warn(
+        "tqdm not installed. This script takes approximately 5 minutes "
+        "to run. To view live progressbars, please install tqdm. "
+        "Otherwise, be patient."
+    )
+
     def tqdm(i, **kwargs):
         return i
 
 
 parser = argparse.ArgumentParser(usage=__doc__)
-parser.add_argument('path', help='Curent directory.')
+parser.add_argument("path", help="Curent directory.")
 args = parser.parse_args()
 
 
 def get_commits(repo, users, reviewers, dir):
-    upcomming_changes_path = os.path.join(dir, 'doc','source', 'upcoming_changes')
+    upcomming_changes_path = os.path.join(dir, "doc", "source", "upcoming_changes")
     prs_list = []
     for file in os.listdir(upcomming_changes_path):
-        pr_number = file.split('.')[0]
-        if os.path.isfile(os.path.join(upcomming_changes_path, file)) and pr_number.isnumeric():
+        pr_number = file.split(".")[0]
+        if (
+            os.path.isfile(os.path.join(upcomming_changes_path, file))
+            and pr_number.isnumeric()
+        ):
             prs_list.append(pr_number)
-    
-    if 'towncrier_template' in prs_list:
-        prs_list.remove('towncrier_template')
+
+    if "towncrier_template" in prs_list:
+        prs_list.remove("towncrier_template")
 
     if not prs_list:
         # this list is empty, no significant changes to adress.
-        print('No significant changes.')
+        print("No significant changes.")
         return
     else:
-        print('Getting all commits from upcomming changes...')
+        print("Getting all commits from upcomming changes...")
 
         all_commits = list()
         for pr_number in prs_list:
@@ -76,9 +83,9 @@ def find_author_info(commit):
     committer = None
     if commit.committer is not None:
         committer = commit.committer.name or commit.committer.login
-    git_author = commit.raw_data['commit']['author']['name']
+    git_author = commit.raw_data["commit"]["author"]["name"]
     if commit.author is not None:
-        author = commit.author.name or commit.author.login + f' ({git_author})'
+        author = commit.author.name or commit.author.login + f" ({git_author})"
     else:
         # Users that deleted their accounts will appear as None
         author = git_author
@@ -94,19 +101,20 @@ def add_to_users(users, new_user):
 
 def main(args):
     dir = args.path
-    GH_USER = 'scikit-image'
-    GH_REPO = 'scikit-image'
-    GH_TOKEN = os.environ.get('GH_TOKEN')
+    GH_USER = "scikit-image"
+    GH_REPO = "scikit-image"
+    GH_TOKEN = os.environ.get("GH_TOKEN")
     if GH_TOKEN is None:
         raise RuntimeError(
             "It is necessary that the environment variable `GH_TOKEN` "
             "be set to avoid running into problems with rate limiting. "
             "One can be acquired at https://github.com/settings/tokens.\n\n"
             "You do not need to select any permission boxes while generating "
-            "the token.")
+            "the token."
+        )
 
     g = Github(GH_TOKEN)
-    repository = g.get_repo(f'{GH_USER}/{GH_REPO}')
+    repository = g.get_repo(f"{GH_USER}/{GH_REPO}")
 
     authors = set()
     reviewers = set()
@@ -117,7 +125,7 @@ def main(args):
 
     try:
         # find_author_info(dir, repository)
-        for commit in tqdm(all_commits, desc='Getting commiters and authors'):
+        for commit in tqdm(all_commits, desc="Getting commiters and authors"):
             committer, author = find_author_info(commit)
             if committer is not None:
                 committers.add(committer)
@@ -129,39 +137,40 @@ def main(args):
                 add_to_users(users, commit.author)
             authors.add(author)
     except TypeError as e:
-        print('No significant changes.')
-        filename = Path('reviewers_and_authors.txt')
-        filename.write_text('')
+        print("No significant changes.")
+        filename = Path("reviewers_and_authors.txt")
+        filename.write_text("")
         return
 
     # this gets found as a commiter
-    committers.discard('GitHub Web Flow')
-    authors.discard('Azure Pipelines Bot')
-
+    committers.discard("GitHub Web Flow")
+    authors.discard("Azure Pipelines Bot")
 
     contributors = OrderedDict()
 
-    contributors['authors'] = authors
+    contributors["authors"] = authors
     # contributors['committers'] = committers
-    contributors['reviewers'] = reviewers
+    contributors["reviewers"] = reviewers
 
-    with open('reviewers_and_authors.txt', 'w+') as file:
+    with open("reviewers_and_authors.txt", "w+") as file:
         for section_name, contributor_set in contributors.items():
-            file.write('\n')
-            committer_str = (f'{len(contributor_set)} {section_name} added to this '
-                     'release [alphabetical by first name or login]\n')
+            file.write("\n")
+            committer_str = (
+                f"{len(contributor_set)} {section_name} added to this "
+                "release [alphabetical by first name or login]\n"
+            )
             file.write(committer_str)
-            file.write('-' * len(committer_str))
-            file.write('\n')
+            file.write("-" * len(committer_str))
+            file.write("\n")
 
             # Remove None from contributor set if it's in there.
             if None in contributor_set:
                 contributor_set.remove(None)
 
             for c in sorted(contributor_set, key=str.lower):
-                file.write(f'- {c} \n')
-            file.write('\n')
+                file.write(f"- {c} \n")
+            file.write("\n")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main(args)
-
